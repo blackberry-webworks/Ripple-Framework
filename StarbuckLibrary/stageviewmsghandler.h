8f3da4b921592b9cd9da754142704e81494ffc94
Eric Pearson <epearson@rim.com>
2011-08-04 17:33:11 -0400

    Implemented reload() method of stagewebview

--- StarbuckLibrary/stageviewmsghandler.h
+++ StarbuckLibrary/stageviewmsghandler.h
@@ -57,6 +57,7 @@ public slots:
 	void processMessage(Message* pMsg);
   void resourceRequest(QNetworkRequest* request);
   void resourceReply(QNetworkReply* reply);
+  void refresh();
 
 //stagewebview events
 signals:
