8061d684da2bfc17d1b807d7e0b94ee8a3cfc15c
Shiwei Tu <stu@rim.com>
2011-11-15 14:02:12 -0500

    added exit error code to fail the build process

--- lint.bat
+++ lint.bat
@@ -1,1 +1,6 @@
+@echo off   
 python cpplint.py --filter=+build,-build/include,+readability,-readability/multiline_comment,-readability/function,+runtime,-runtime/int,-runtime/printf,-runtime/rtti,-whitespace --output=vs7 %1 %2
+
+if errorlevel 1 exit /b  1
+
+
--- lintFiles.bat
+++ lintFiles.bat
@@ -1,14 +1,23 @@
+@echo off
 call lint.bat --R Ripple
+if errorlevel 1 exit 1
 call lint.bat --R RippleLibrary
+if errorlevel 1 exit 1
 call lint.bat TestClientTest/Main.cpp
+if errorlevel 1 exit 1
 call lint.bat TestFramework/Main.cpp
+if errorlevel 1 exit 1
 call lint.bat TestFramework/MessageHandlerTest.cpp
+if errorlevel 1 exit 1
 call lint.bat TestFramework/MessageHandlerTest.h
+if errorlevel 1 exit 1
 call lint.bat TestFramework/MessageSignalSpy.h
+if errorlevel 1 exit 1
 call lint.bat TestFramework/QtStageWebViewTest.cpp
+if errorlevel 1 exit 1
 call lint.bat TestFramework/QtStageWebViewTest.h
-exit(1)
-
+if errorlevel 1 exit 1
+@echo on
 
 
 
