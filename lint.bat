324627022a74f3245a385312b498d1f99433df3d
Eric Pearson <epearson@rim.com>
2011-11-17 11:47:00 -0500

    Updated lint rules

--- lint.bat
+++ lint.bat
@@ -1,5 +1,5 @@
 @echo off   
-python cpplint.py --filter=+build,-build/include,+readability,-readability/multiline_comment,-readability/function,+runtime,-runtime/int,-runtime/printf,-runtime/rtti,-whitespace/braces,-whitespace/line_length,-whitespace/comments --output=vs7 %1 %2
+python cpplint.py --filter=+build,-build/include,+readability,-readability/multiline_comment,-readability/function,+runtime,-runtime/int,-runtime/printf,-runtime/rtti,-whitespace/braces,-whitespace/line_length,-whitespace/comments,-whitespace/labels --output=vs7 %1 %2
 
 if errorlevel 1 exit /b  1
 
