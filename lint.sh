35b9089c88e14d013e4d6a62e200ac084e65179a
Eric Pearson <epearson@rim.com>
2011-11-17 12:24:01 -0500

    Updated lint script

--- lint.sh
+++ lint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-    find $1 -type f -iname \*.cc -or -iname \*.h -exec sh -c  "python cpplint.py  --filter=+build,-build/include,+readability,-readability/multiline_comment,-readability/function,+runtime,-runtime/int,-runtime/printf,-runtime/rtti,-whitespace --output=vs7 '{}'" \; 
+    find $1 -type f -iname \*.h -exec sh -c  "python cpplint.py  --filter=+build,-build/include,+readability,-readability/multiline_comment,-readability/function,+runtime,-runtime/int,-runtime/printf,-runtime/rtti,-whitespace/braces,-whitespace/line_length,-whitespace/comments,-whitespace/parens --output=vs7 '{}'" \; 
 
+    find $1 -type f -iname \*.cpp -exec sh -c  "python cpplint.py  --filter=+build,-build/include,+readability,-readability/multiline_comment,-readability/function,+runtime,-runtime/int,-runtime/printf,-runtime/rtti,-whitespace/braces,-whitespace/line_length,-whitespace/comments,-whitespace/parens --output=vs7 '{}'" \; 
