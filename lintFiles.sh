486c3fae2b79b4ccc58ffcc28486da23b7d6d5e4
Eric Pearson <epearson@rim.com>
2011-11-21 14:42:13 -0500

    Add new files to mac lint

--- lintFiles.sh
+++ lintFiles.sh
@@ -8,3 +8,7 @@ sh lint.sh TestFramework/MessageHandlerTest.h
 sh lint.sh TestFramework/MessageSignalSpy.h
 sh lint.sh TestFramework/QtStageWebViewTest.cpp
 sh lint.sh TestFramework/QtStageWebViewTest.h
+sh lint.sh TestFramework/NetworkAccessManagerTest.cpp
+sh lint.sh TestFramework/NetworkAccessManagerTest.h
+sh lint.sh TestFramework/ResourceRequestedReplyTest.cpp
+sh lint.sh TestFramework/ResourceRequestedReplyTest.h
