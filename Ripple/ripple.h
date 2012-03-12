6913843231d189396f831113c661468e8660d05d
pagey <epearson@rim.com>
2012-03-12 16:23:06 -0400

    Removed menu bar

--- Ripple/ripple.h
+++ Ripple/ripple.h
@@ -47,11 +47,8 @@ namespace BlackBerry
     private slots:
         void registerAPIs();
         void urlChanged(QUrl &url);
-        void toggleHardwareAcceleration();
 
     private:
-        QMenu *_optionsMenu;
-        QAction *_hwToggleMenuItem;
         QGLWidget *_GLWidget;
         ScrollHandler *_scrollHandler;
         static const int PROGRESS_BAR_HEIGHT;
